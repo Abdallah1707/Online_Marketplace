6bf26c5067806ad8c05482802ea21659a2abc172
Abdallah1707 <abdallahmomen1707@gmail.com>
2025-12-09 15:59:14 +0200

    added get all users

--- marketplace-api/controllers/authController.js
+++ marketplace-api/controllers/authController.js
@@ -24,3 +24,10 @@ exports.login = async (req, res, next) => {
     res.json({ token });
   } catch (err) { next(err); }
 };
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find().select('-password');
+    res.json(users);
+  } catch (err) { next(err); }
+};
